c63498a63a14852557af7999b877dc14100ef47c
idevemohan <sharewithdeve@gmail.com>
2025-08-14 12:32:10 +0530

    second commit

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,23 @@ import './index.css'
 import App from './App.jsx'
 import { BookProvider } from "./pages/BookContext"
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
+const container = document.getElementById('root');
+const root = createRoot(container);
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <BookProvider>
-    <App />
-  </BookProvider>
+
+// ReactDOM.createRoot(document.getElementById("root"))
+
+root.render(
+  <StrictMode>
+    <BookProvider>
+      <App />
+    </BookProvider>
+
+  </StrictMode>
+
 );
